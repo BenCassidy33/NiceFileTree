2fef2a91295f9e9b303712a04de96c65352069c3
Benjamin Cassidy <ben.d.cassidy@gmail.com>
2025-12-09 20:26:30 -0500

    update

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
 local async = require("plenary.async")
+local icons = require("nvim-web-devicons")
 
 Plugin = {
-    options = {
-        showHidden = true
-    }
+	options = {
+		showHidden = true,
+		showIcons = true,
+	},
 }
 
 ---@class TimeSpec
@@ -32,27 +34,29 @@ Plugin = {
 ---@class FileInfo
 ---@field filepath string
 ---@field filename string
+---@field extension string
 ---@field stat FileStat
-
+---
 ---@class State
 ---@field workingDirecory string
 ---@field isSideBarOpen boolean
+---@field openDirectories string[]
 ---@field files FileInfo[]
 ---@field directories FileInfo[]
 
 ---@class State
 local state = {
 	workingDirecory = vim.fn.getcwd(),
-
 	isSideBarOpen = false,
-
 	files = {},
 	directories = {},
-
+    openDirectories = {},
 	bufno = nil,
 	winno = nil,
+    lastbuf = nil,
 }
 
+---@param filename string
 ---@return FileInfo
 local getFileInfo = function(filename)
 	local filepath = state.workingDirecory .. "/" .. filename
@@ -60,13 +64,14 @@ local getFileInfo = function(filename)
 		filepath = filepath,
 		filename = filename,
 		stat = nil,
+		extension = nil,
 	}
-
-	print("Reading " .. file_info.filepath)
 
 	local _, fd = async.uv.fs_open(filepath, "r", 438)
 	local _, stat = async.uv.fs_fstat(fd)
 	file_info.stat = stat
+
+	file_info.extension = filename:match("%.([^%.]+)$")
 
 	async.uv.fs_close(fd)
 	return file_info
@@ -87,67 +92,147 @@ local getDirInfo = function()
 	end
 end
 
-local createSidebar = function()
-	vim.cmd("vsplit")
-	state.winno = vim.api.nvim_get_current_win()
-	state.bufno = vim.api.nvim_create_buf(true, true)
-	vim.api.nvim_win_set_buf(state.winno, state.bufno)
-    state.isSideBarOpen = true
+local handleBufUpdate = function()
+	local lines = vim.api.nvim_buf_get_lines(state.bufno, 0, -1, false)
+	vim.api.nvim_set_option_value("modified", false, { buf = state.bufno })
+    print(vim.inspect(lines), vim.inspect(lines))
 end
 
-local writeBuffer = function()
+local updateBuffer = function()
 	local text = {}
+	local linenum = 0
+
 	for _, file in ipairs(state.directories) do
-		text[#text + 1] = file.filename .. "/"
+		text[#text + 1] = " " .. file.filename .. "/"
+		linenum = linenum + 1
 	end
 
 	for _, file in ipairs(state.files) do
 		text[#text + 1] = file.filename
+		linenum = linenum + 1
 	end
 
-    vim.schedule(function ()
-	    vim.api.nvim_buf_set_lines(state.bufno, 0, -1, false, text)
-    end)
+	vim.schedule(function()
+		vim.api.nvim_buf_set_lines(state.bufno, 0, -1, false, text)
+        state.lastbuf = text
+
+		local ns_id = vim.api.nvim_create_namespace("iconcol")
+
+		if Plugin.options.showIcons then
+			linenum = 0
+
+			for _, dir in ipairs(state.directories) do
+				local icon = icons.get_icon(dir.filename, dir.extension, { default = true })
+				vim.api.nvim_buf_set_extmark(state.bufno, ns_id, linenum, 0, {
+					virt_text = { { icon .. " ", "Normal" } },
+					virt_text_pos = "inline",
+				})
+
+				linenum = linenum + 1
+			end
+
+			for _, file in ipairs(state.files) do
+				local icon = icons.get_icon(file.filename, file.extension, { default = true })
+
+				vim.api.nvim_buf_set_extmark(state.bufno, ns_id, linenum, 0, {
+					virt_text = { { icon .. " ", "Normal" } },
+					virt_text_pos = "inline",
+				})
+
+				linenum = linenum + 1
+			end
+		end
+
+		vim.api.nvim_set_option_value("modified", false, { buf = state.bufno })
+	end)
+end
+
+local closeSidebar = function()
+	if not state.bufno and not state.winno then
+		return
+	end
+
+	local ns_id = vim.api.nvim_create_namespace("iconcol")
+	vim.api.nvim_buf_clear_namespace(state.bufno, ns_id, 0, -1)
+
+	if vim.api.nvim_win_is_valid(state.winno) then
+		vim.api.nvim_win_close(state.winno, true)
+	end
+
+	if vim.api.nvim_buf_is_valid(state.bufno) then
+		vim.api.nvim_buf_delete(state.bufno, { force = true })
+	end
+
+
+	state.winno = nil
+	state.bufno = nil
+	state.isSideBarOpen = false
+	state.files = {}
+	state.directories = {}
+end
+
+local createSidebar = function()
+	state.isSideBarOpen = true
+
+	vim.cmd("vsplit")
+
+	state.winno = vim.api.nvim_get_current_win()
+	state.bufno = vim.api.nvim_create_buf(true, true)
+
+	vim.api.nvim_win_set_buf(state.winno, state.bufno)
+
+	vim.api.nvim_buf_set_name(state.bufno, "slicktree://sidebar")
+
+	vim.api.nvim_set_option_value("buftype", "acwrite", { buf = state.bufno })
+	vim.api.nvim_set_option_value("modifiable", true, { buf = state.bufno })
+
+	vim.api.nvim_create_autocmd("BufWriteCmd", {
+		buffer = state.bufno,
+		callback = function()
+			handleBufUpdate()
+		end,
+	})
 end
 
 local sidebar = function(toggle_type)
-    if (toggle_type == "toggle" or toggle_type == "close") and state.isSideBarOpen == true then
-        vim.api.nvim_win_close(state.winno, true)
+	print(toggle_type, state.isSideBarOpen)
 
-        state.winno = nil
-        state.bufno = nil
-        state.isSideBarOpen = false
-        state.files = {}
-        state.directories = {}
-    elseif (toggle_type == "open" or toggle_type == "toggle" or toggle_type == "focus") and state.isSideBarOpen == false then
-        async.run(function()
-            getDirInfo()
-        end, function()
-            writeBuffer()
-        end)
+	if state.isSideBarOpen then
+		if toggle_type == "toggle" or toggle_type == "close" then
+			closeSidebar()
+			return
+		end
 
-        createSidebar()
-    else
-        vim.api.nvim_set_current_win(state.winno)
-    end
+		if toggle_type == "focus" or toggle_type == "open" then
+			vim.api.nvim_set_current_win(state.winno)
+		end
+	elseif not state.isSideBarOpen then
+		async.run(function()
+			getDirInfo()
+		end, function()
+			updateBuffer()
+		end)
+
+		createSidebar()
+	end
 end
 
-Plugin.OpenSidebar = function ()
-    sidebar("open")
+Plugin.OpenSidebar = function()
+	sidebar("open")
 end
 
-Plugin.CloseSidebar = function ()
-    sidebar("close")
+Plugin.CloseSidebar = function()
+	sidebar("close")
 end
 
-Plugin.ToggleSidebar = function ()
-    sidebar("toggle")
+Plugin.ToggleSidebar = function()
+	sidebar("toggle")
 end
 
-Plugin.FocusSidebar = function ()
-    sidebar("focus")
+Plugin.FocusSidebar = function()
+	sidebar("focus")
 end
 
-vim.keymap.set("n", "<leader>S", function ()
-    Plugin.ToggleSidebar()
+vim.keymap.set("n", "<leader>S", function()
+	Plugin.ToggleSidebar()
 end)
